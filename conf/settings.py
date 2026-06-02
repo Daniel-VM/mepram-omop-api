49c7428548bbf22de106d1ff7122a1b1f96083d5
ESapenaVentura <sapenaventura@gmail.com>
2026-06-02 13:37:00 +0200

    Updated settings to not trim prefix

--- conf/settings.py
+++ conf/settings.py
@@ -87,7 +87,6 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "v1",
     "SERVE_INCLUDE_SCHEMA": True,
     "SCHEMA_PATH_PREFIX": "/v1",
-    "SCHEMA_PATH_PREFIX_TRIM": True,
     "SERVERS": [{"url": "/v1", "description": "MePRAM API v1"}],
     "SORT_OPERATIONS": False,
 }
