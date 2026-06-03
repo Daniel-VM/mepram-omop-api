430f4e92478e5cb30e3a7f1dd1d892dec0b33430
ESapenaVentura <sapenaventura@gmail.com>
2026-06-03 11:44:17 +0200

    fixing path trimming

--- conf/urls.py
+++ conf/urls.py
@@ -4,10 +4,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="swagger-ui", permanent=False)),
-    path("/openapi", SpectacularAPIView.as_view(url="/openapi/"), name="schema"),
+    path("openapi/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
+        SpectacularSwaggerView.as_view(url="/openapi/"),
         name="swagger-ui",
     ),
     path("v1/", include("core.api.v1.urls")),
