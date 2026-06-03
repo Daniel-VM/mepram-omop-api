0234f44ba72e5922bf593f3b3a7c10b7998a6f59
ESapenaVentura <sapenaventura@gmail.com>
2026-06-03 12:08:04 +0200

    testing

--- conf/urls.py
+++ conf/urls.py
@@ -3,12 +3,11 @@ from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="swagger-ui1", permanent=False)),
-    path("openapi/", SpectacularAPIView.as_view(), name="schema"),
+    path("", RedirectView.as_view(pattern_name="swagger-ui", permanent=False)),
     path(
         "swagger/",
         SpectacularSwaggerView.as_view(url="/openapi/"),
-        name="swagger-ui1",
+        name="swagger-ui",
     ),
     path("v1/", include("core.api.v1.urls")),
 ]
